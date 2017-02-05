4aa7c8658f07a7e59488665ad720a5c591863ebc
nenravitsa <melioraa@gmail.com>
2017-02-05 22:21:36 +0300

    add class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
 
-    setRange(min, max) {
-
+    constructor() {
+        this.low = 0;
+        this.high = 0;
+        this.mid = 0;
     }
 
+    setRange(min, max) {
+        this.low = min;
+        this.high = max;
+    }
     guess() {
-
+        var result = Math.floor((this.high - this.low+1) / 2);
+        return this.mid = result+this.low;
     }
 
     lower() {
-
+        this.setRange(this.low, this.mid);
     }
 
     greater() {
-
+        this.setRange(this.mid,this.high);
     }
 }
 
